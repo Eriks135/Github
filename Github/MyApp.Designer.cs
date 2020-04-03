03f42c6abbad868e909ae5d591298c9b54a2687d
Erik Kancs <kancs.eriks@gmail.com>
2020-04-03 17:06:41 +0300

    Added Hello button

--- Github/MyApp.Designer.cs
+++ Github/MyApp.Designer.cs
@@ -40,6 +40,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -50,14 +51,14 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // MyApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(503, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "MyApp";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
